3ed2b00dd509fef1da190e51b455c83b16954aee
Keshav Somani <75680431+keshavs03@users.noreply.github.com>
2022-02-04 12:31:02 +0530

    removed error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Contact from "./Contact";
-import adiImage from "./../public/adizya.svg";
 export default function Home() {
   return (
     <div>
