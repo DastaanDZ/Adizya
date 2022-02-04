304f0b7c8d73d5cf4ebd39d1b1cac8116564bb46
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-02-05 03:30:24 +0530

    made lecture page responsive

--- pages/lectures/index.js
+++ pages/lectures/index.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
+import Link from "next/link"
 
 import Sidebar from "../../components/sidebar/Sidebar"
 import Larr from "../../assets/Larr"
 import Lec1 from "./Lec1"
 import Form from '../../components/registerform'
+import icon from '../../assets/images/icon.png'
 
 import styles from '../lectures/lectures.module.css'
 
@@ -16,6 +18,12 @@ const index = () => {
     return (
     <>
         <div className={styles.container}>
+            <div className={styles.mobile_top_div}>
+            <Link href='/'> 
+      <img src={icon.src} alt="xxx" className={styles.icon} />
+    </Link>
+    <p className={styles.login}>Login</p> 
+            </div>
             <Sidebar sidebar={sidebar}/>
             <div className={styles.page}> 
                 <div className={styles.open}> 
