a8ee9c1d644721553fb54974e55559d6e61e6d65
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-02-04 23:14:36 +0530

    created landing page and  made svg animation

---
+++ pages/landing/index.js
@@ -0,0 +1,20 @@
+import Hero from "../../assets/Hero"
+import icon from '../../assets/images/icon.png'
+
+import styles from '../landing/landing.module.css'
+
+const index = () => {
+  return (
+    <>
+    <div className={styles.container}> 
+    <img src={icon.src} alt="xxx" className={styles.icon}/>
+    <p className={styles.login}>Login</p>
+    <div className={styles.hero_container}> 
+       <Hero/> 
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default index
