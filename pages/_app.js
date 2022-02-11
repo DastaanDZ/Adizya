c4b1d26b38cf1538809ff1f918b4613d3582b4ef
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-02-11 20:44:27 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,33 @@
 import "../styles/globals.css";
 import "../styles/form.css";
 import "../styles/contact.css";
+
 import UserContextProvider from "@/context/userContext";
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+          <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+          });
+    `}
+      </Script>
+
     <UserContextProvider>
       <Component {...pageProps} />{" "}
     </UserContextProvider>
+    </>
   );
 }
 
