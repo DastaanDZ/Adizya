87cb11c4149e8b297d23f25a9e50698a93c746db
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-02-12 11:00:08 +0530

    contact ready to deploy

--- components/maps/index.js
+++ components/maps/index.js
@@ -5,9 +5,9 @@ import styles from './maps.module.css'
 const Index = () => {
   return (
     <>
-        <div class={styles.mapouter}>
-        <div class={styles.gmap_canvas}>
-        <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=National%20Institute%20of%20Technology,%20Calicut&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+        <div className={styles.mapouter}>
+        <div className={styles.gmap_canvas}>
+        <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=National%20Institute%20of%20Technology,%20Calicut&t=&z=13&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0">
 
         </iframe>
         <a href="https://putlocker-is.org"></a>
