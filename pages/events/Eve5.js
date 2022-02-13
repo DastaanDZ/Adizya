c78f030085f3ca4708f697cba9d4ac4e103b54f6
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-02-13 16:33:12 +0530

    updated metafolio

--- pages/events/Eve5.js
+++ pages/events/Eve5.js
@@ -7,7 +7,7 @@ const Eve5 = () => {
     <>
       <div>
         <div className={styles.title}>
-          <h1 className={styles.h1}>Confero Veritas</h1>
+          <h1 className={styles.h1}>Metafolio</h1>
           <hr className={styles.hr} />
         </div>
         <div className={styles.info}>
@@ -17,37 +17,40 @@ const Eve5 = () => {
             </div>
           </div>
           <p className={styles.desc}>
-            Event Description: Participants will be given a series of questions
-            which need to be answered with relevant pictures. The participant
-            with the most number of relevant pictures will be declared the
-            winner. Guidelines:
+          In this fast paced world of advanced technology at fingertips, should architects stick to the traditional methods? We offer you a chance to showcase your creativity and enhance your portfolio while stepping into the alternate realm of the virtual worlds through Metafolio. Adizya’21 brings to you Metafolio Design Hackathon - a residence design competition, presented by Buildnext. 
             <ul>
-              <li>● The picture should be taken by the participant only</li>
+              <li>●	This is a 3D modelling/designing event. Participants should create a 360 visualisation zip file using any software.</li>
               <li>
-                ● The picture should not be taken from an external source like
-                Google/Pinterest.
+              ●	This event is open to architecture students of all years.
               </li>
               <li>
-                ● Each question will have a time limit of 15 - 20 minutes,
-                within which the answer needs to be submitted.{" "}
+              ●	This is a submission event. participants should upload one 360 render zip file and one full hd render have to be submitted in google form at adizya.metafolio.in.
               </li>
+              <li>●	At the time submission one has to mention the short description of the model and how effectively design strategies have been used.</li>
+              <li>●	This is an individual event.</li>
+              <li>●	Any software can be used to render and make a 360 visualisation zip file.</li>
+              <li>●	Innovativeness is the major judging criteria.</li>
             </ul>
           </p>
 
           <p className={styles.desc_info}>
             Prizes Worth:<br></br>
-            1st Place -1k, 2nd Place - 500, 3rd Place - 200
+            Jury choice awards: NFT rewards<br></br>
+People's choice awards: NFT rewards<br></br>
+internships / pre-placement interviews<br></br>
+Participants: NFT certificates 
+
           </p>
 
           <p className={styles.desc_info}>
             Contact<br></br>
-            Jithin A.J - +91 97444 89606, <br></br>
-            Vaisakh - +91 89430 76247<br></br>
+            Aavas - +91 8943076247<br></br>
+            Keerthana - +91 9962817804<br></br>
           </p>
         </div>
         <div className={styles.btn_container}>
           <a
-            href="https://forms.gle/EpstPvUabXcHjaoC9"
+            href="https://adizya.metafolio.in/"
             className={styles.register_btn}
           >
             REGISTER
